21e3f033a2935193a9015dbac83401280654c3c1
Vanesha-Morningstar <irvanpplg1@gmail.com>
2024-10-24 11:29:50 +0700

    second

--- lib/main.dart
+++ lib/main.dart
@@ -15,46 +15,110 @@ class MyApp extends StatelessWidget {
 class MenuPage extends StatelessWidget {
   final List<Map<String, String>> menuItems = [
     {
-      'judul'   : 'Price List Epic - Legend',
-      'gambar'  : 'gr.jpeg',
-      'rating'  : '4.9',
-      'waktu'   : '1 second',
-      'review'  : '4,999 rb',
+      'judul': 'Granger Exorcist',
+      'gambar': 'assets/gr.jpeg',
+      'rating': '4.9',
+      'waktu': '1 second',
+      'review': '4,999 rb',
     },
     {
-      'judul'   : 'Price List Epic - Legend',
-      'gambar'  : 'gs.jpeg',
-      'rating'  : '4.9',
-      'waktu'   : '1 second',
-      'review'  : '4,999 rb',
+      'judul': 'Gusion 11.11',
+      'gambar': 'assets/gs.jpeg',
+      'rating': '4.9',
+      'waktu': '1 second',
+      'review': '4,999 rb',
     },
     {
-      'judul'   : 'Price List Epic - Legend',
-      'gambar'  : 'nolan.jpeg',
-      'rating'  : '4.9',
-      'waktu'   : '1 second',
-      'review'  : '4,999 rb',
+      'judul': 'Leomord Ducati',
+      'gambar': 'assets/leo.jpeg',
+      'rating': '4.9',
+      'waktu': '1 second',
+      'review': '4,999 rb',
     },
     {
-      'judul'   : 'Price List Epic - Legend',
-      'gambar'  : 'linglung.jpeg',
-      'rating'  : '4.9',
-      'waktu'   : '1 second',
-      'review'  : '4,999 rb',
+      'judul': 'Ling Collector',
+      'gambar': 'assets/linglung.jpeg',
+      'rating': '4.9',
+      'waktu': '1 second',
+      'review': '4,999 rb',
     }
   ];
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: ButtonBar(
-          children: [
-            IconButton(onPressed: (){}, icon: Icon(Icons.chevron_left))
+        
+        title: Text('Price List Gift Skin MLBB'),
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.deepPurple, 
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundImage: AssetImage('assets/glarr.jpeg'), 
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Jasa Joki Mobile Legends',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Fast, Trusted, Affordable',
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.military_tech),
+              title: Text('Rank Boosting'),
+              onTap: () {
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.speed),
+              title: Text('Fast Win Service'),
+              onTap: () {
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.contact_phone),
+              title: Text('Contact Us'),
+              onTap: () {
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+              onTap: () {
+              },
+            ),
           ],
         ),
-        title: Text('Price List Epic - Legend'),
-        centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
@@ -63,10 +127,10 @@ class MenuPage extends StatelessWidget {
             crossAxisCount: 2,
             crossAxisSpacing: 10,
             mainAxisSpacing: 10,
-            childAspectRatio: 0.5,
+            childAspectRatio: 0.6,
           ),
           itemCount: menuItems.length,
-          itemBuilder: (context,index) {
+          itemBuilder: (context, index) {
             return buildMenuItem(menuItems[index]);
           },
         ),
@@ -75,154 +139,159 @@ class MenuPage extends StatelessWidget {
   }
 
   Widget buildMenuItem(Map<String, String> item) {
-  var width = 20;
-  return Card(
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(15),
-    ),
-    elevation: 3,
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(5),
-              topRight: Radius.circular(5)),
+    var width = 20;
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      elevation: 3,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(5), topRight: Radius.circular(5)),
+            ),
+            child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(1000),
+                      child: Image.asset(
+                        'assets/glarr.jpeg',
+                        width: 35,
+                        height: 35,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'Glarr Store',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 15,
+                      ),
+                    ),
+                    SizedBox(width: 5),
+                    CircleAvatar(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                      radius: 7,
+                      child: Icon(
+                        Icons.check,
+                        size: 10,
+                      ),
+                    )
+                  ],
+                )),
           ),
-          child: Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Row(
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.network(
+              item['gambar']!,
+              height: 180,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(1000),
-                  child: Image.asset(
-                    'lom ada',
-                    width: 20,
-                    height: 20,
+                Text(
+                  item['judul']!,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
                   ),
                 ),
-                SizedBox(width: 5,),
-                Text(
-                  'lom ada',
-                  textAlign: TextAlign.center,
-                  style:
-                  TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    ),
+                SizedBox(height: 5),
+                Row(
+                  children: [
+                    Icon(Icons.star, color: Colors.orange, size: 16),
+                    SizedBox(width: 5),
+                    Text(item['rating']!, style: TextStyle(fontSize: 14)),
+                    Spacer(),
+                    Icon(Icons.timer, size: 16, color: Colors.grey),
+                    SizedBox(width: 5),
+                    Text(item['waktu']!, style: TextStyle(fontSize: 14)),
+                  ],
                 ),
-                SizedBox(width: 5),
-                CircleAvatar(
-                  backgroundColor: Colors.blue,
-                  foregroundColor: Colors.white,
-                  radius: 7,
-                  child: Icon(
-                    Icons.check,
-                    size: 10,
+                SizedBox(height: 5),
+                Row(
+                  children: [
+                    Icon(Icons.visibility, size: 16, color: Colors.grey),
+                    SizedBox(width: 5),
+                    Text(item['review']!, style: TextStyle(fontSize: 14)),
+                    Spacer(),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(1000),
+                          boxShadow: [
+                            BoxShadow(color: Colors.grey, blurRadius: 4)
+                          ]),
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.bookmark_border,
+                            size: 20,
+                          )),
+                    )
+                  ],
                 ),
-                )
               ],
-            )
+            ),
           ),
-        ),
-      ClipRRect(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
-        child: Image.network(
-          item['gambar']!,
-          height: 180,
-          width: double.infinity,
-          fit: BoxFit.cover,
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              item['judul']!,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 16,
-              ),
-            ),
-            SizedBox(height: 5),
-            Row(
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(
-                  Icons.star,
-                  color: Colors.orange,
-                  size: 16
-                ),
-                SizedBox(width: 5),
-                Text(
-                  item['rating']!,
-                  style: TextStyle(
-                    fontSize: 14
-                  )
-                ),
-                Spacer(),
-                Icon(
-                  Icons.timer,
-                  size: 16,
-                  color: Colors.grey
-                ),
-                SizedBox(width: 5),
-                Text(
-                  item['waktu']!,
-                  style: TextStyle(
-                    fontSize: 14
-                  )
-                ),
-              ],
-            ),
-            SizedBox(height: 5),
-            Row(
-              children: [
-                Icon(
-                  Icons.visibility,
-                  size: 16,
-                  color: Colors.grey
-                ),
-                SizedBox(width: 5),
-                Text(
-                  item['review']!,
-                  style: TextStyle(
-                    fontSize: 14
-                  )
-                ),
-                Spacer(),
                 Container(
+                  width: 75,
                   decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(1000),
                     boxShadow: [
                       BoxShadow(
+                        blurRadius: 4,
                         color: Colors.grey,
-                        blurRadius: 4
-                      )
-                    ]
+                      ),
+                    ],
                   ),
-                  child: IconButton(
-                    onPressed: (){},
-                    icon: Icon(
-                      Icons.bookmark_border,
-                      size: 20,
-                    )
+                  child: Row(
+                    // Gunakan Column agar Icon dan Text tersusun secara vertikal
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Icon(
+                      //   Icons.shopping_cart, // Ganti dengan ikon yang diinginkan
+                      //   size: 14,             // Ukuran ikon
+                      //   color: Colors.black, // Warna ikon
+                      // ),
+                      Text(
+                        'Order',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
                   ),
                 )
               ],
             ),
-          ],
-        ),
+          )
+        ],
       ),
-      ],
-    ),
-  );
+    );
+  }
 }
-}
-
